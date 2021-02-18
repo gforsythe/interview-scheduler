a1359ade9e2cb0722952c582b189631161158e10
Gabriel Forsythe <gabe.forsythe@gmail.com>
2021-02-17 23:10:52 -0800

    adding files and removed data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,53 +1,101 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "components/DayList";
+import Appointment from "components/Appointment";
 
 
-
-const days = [
+const appointments = [
   {
     id: 1,
-    name: "Monday",
-    spots: 2,
+    time: "12pm",
   },
   {
     id: 2,
-    name: "Tuesday",
-    spots: 5,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
   },
   {
     id: 3,
-    name: "Wednesday",
-    spots: 0,
+    time: "2pm",
+    interview: {
+      student: "Willow Chown",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      }
+    }
   },
+  {
+    id: 8,
+    time: "3pm",
+  },
+  {
+    id: 4,
+    time: "4pm",
+    interview: {
+      student: "Bianca Weima",
+      interviewer: {
+        id: 3,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      }
+    }
+  }
 ];
 
-
 export default function Application(props) {
+  const [days, setDays] = useState([]);
   const [day, setDay] = useState("Monday");
-  console.log(day);
+
+  useEffect(() => {
+    const url="/api/days"
+    axios.get(url)
+    .then(function (response) {
+      setDays([...response.data]);
+    })
+  }, [])
+  
+  const mappedAppointments = appointments.map((appointment) => {
+    return (
+      <Appointment
+        key={appointment.id}
+        {...appointment}
+      />
+    );
+  });
+
   return (
     <main className="layout">
       <section className="sidebar">
         <img
-  className="sidebar--centered"
-  src="images/logo.png"
-  alt="Interview Scheduler"
-/>
-<hr className="sidebar__separator sidebar--centered" />
-<nav className="sidebar__menu">
-<DayList
-  days={days}
-  day={day}
-  setDay={setDay}
-/>
-</nav>
-<img
-  className="sidebar__lhl sidebar--centered"
-  src="images/lhl.png"
-  alt="Lighthouse Labs"
-/>
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          <DayList days={days} day={day} setDay={setDay} />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
+      </section>
+
+      <section className="schedule">
+        {mappedAppointments}
+        <Appointment key="last" time="5pm"></Appointment>
       </section>
     </main>
   );
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,32 +1,23 @@
 import React from "react";
-import InterviewerListItem from "components/InterviewerListItem"
+import InterviewerListItem from "components/InterviewerListItem";
 import "components/InterviewerList.scss";
 
-
-
-
-export default function InterviewerList(props){
-
-  const mappedList = props.interviewers.map(interviewer => {
-    return(
+export default function InterviewerList(props) {
+  const mappedList = props.interviewers.map((interviewer) => {
+    return (
       <InterviewerListItem
         key={interviewer.id}
-        name={interviewer.name} 
-        avatar={interviewer.avatar} 
+        name={interviewer.name}
+        avatar={interviewer.avatar}
         selected={interviewer.id === props.interviewer}
-        setInterviewer={ () => props.setInterviewer(interviewer.id)}
-        />
+        setInterviewer={() => props.setInterviewer(interviewer.id)}
+      />
     );
   });
-    return(
-
-      <section className="interviewers">
+  return (
+    <section className="interviewers">
       <h4 className="interviewersheader text--light">Interviewer</h4>
       <ul className="interviewerslist">{mappedList}</ul>
     </section>
-
-
-    )
+  );
 }
-
-
